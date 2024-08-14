95339f2f223149de44a52ce646d0ac07dd6cfc92
Madli <madli.petuhhov89@gmail.com>
2024-08-14 14:39:20 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <NavigationBar/>
   </nav>
   <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script setup>
 
-nav {
-  padding: 30px;
-}
+import NavigationBar from "@/components/NavigationBar.vue";
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar">
+    <ul class="navbar-list">
+      <li class="navbar-item">
+        <router-link to="/">Home</router-link>
+      </li>
+      <li class="navbar-item">
+        <router-link to="/about">About</router-link>
+      </li>
+      <li class="navbar-item">
+        <router-link to="/contact">Contact</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavigationBar"
+}
+</script>
+
+<style scoped>
+
+</style>
