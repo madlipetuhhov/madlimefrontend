94d5d1b66b377e9009dfbc751e977049e5ebf2b2
Madli <madli.petuhhov89@gmail.com>
2024-08-17 13:16:54 +0300

    navbar style WIP

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,20 +1,20 @@
 <template>
   <nav class="navbar-nav">
     <ul class="navbar-list">
-      <li class="navbar-link">
-        <router-link to="/portfoolio">Portfoolio</router-link>
+      <li class="navbar-item">
+        <router-link class="navbar-link" to="/portfoolio">Portfoolio</router-link>
       </li>
-      <li class="navbar-link">
-        <router-link to="/minust">Minust</router-link>
+      <li class="navbar-item">
+        <router-link class="navbar-link" to="/minust">Minust</router-link>
       </li>
-      <li class="navbar-logo">
-        <router-link to="/">MADLI</router-link>
+      <li class="navbar-item">
+        <router-link class="navbar-link logo" to="/">MADLI</router-link>
       </li>
-      <li class="navbar-link">
-        <router-link to="/hind">Info & hind</router-link>
+      <li class="navbar-item">
+        <router-link class="navbar-link" to="/hind">Info & hind</router-link>
       </li>
-      <li class="navbar-link">
-        <router-link to="/kontakt">Kontakt</router-link>
+      <li class="navbar-item">
+        <router-link class="navbar-link" to="/kontakt">Kontakt</router-link>
       </li>
     </ul>
   </nav>
@@ -26,12 +26,33 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .navbar-list{
   list-style: none;
   display: flex;
   align-items: center;
   gap: 4.8rem;
+}
+
+.navbar-link:link,
+.navbar-link:visited {
+  display: inline-block;
+  text-decoration: none;
+  text-transform: uppercase;
+  color: #1F1B22;
+  font-weight: 500;
+  font-size: 1.8rem;
+  transition: all 0.3s;
+}
+
+.navbar-link:hover,
+.navbar-link:active {
+  color: #9B89A9;
+}
+
+.navbar-link.logo{
+  letter-spacing: 2rem;
+  font-size: 6.2rem;
 }
 
 </style>
