5a6d7d8c6dbcfb1e8871a6c6ceb8eeb752aaf95c
Madli <madli.petuhhov89@gmail.com>
2024-08-17 13:40:13 +0300

    hero WIP

---
+++ src/components/Hero.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="section-hero">
+    <div class="hero">
+      <div class="hero-img-box">
+        <img src="">
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Hero"
+}
+</script>
+
+<style scoped>
+
+</style>
