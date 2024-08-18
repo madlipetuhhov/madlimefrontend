2824f92ed450fa8b2554d54f3f0e32789d258bdd
Madli <madli.petuhhov89@gmail.com>
2024-08-18 11:51:21 +0300

    testimonial WIP

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="testimonials">
-    <figure class="testimonial">
-      <img class="testimonial-img" src="../assets/img/testimonial/testimonial1.png" alt="Portrait of client."/>
+    <figure class="testimonial" v-for="(testimonial, index) in testimonials"
+            :key="index" v-show="index === currentTestimonial">
+      <img class="testimonial-img" :src="testimonial.img" :alt="testimonial.alt"/>
       <blockquote>
-        <p class="testimonial-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lorem velit,
-          vulputate eu lacus ut, dictum tempor.</p>
-        <p class="testimonial-name">&mdash; Mari Maasikas</p>
+        <p class="testimonial-text">{{ testimonial.text }}</p>
+        <p class="testimonial-name">&mdash; {{ testimonial.name }}</p>
       </blockquote>
     </figure>
-    <button class="btn btn--left">
+    <button class="btn btn--left" @click="previousTestimonial">
       <PhCaretLeft :size="32" weight="light" class="btn-icon"/>
     </button>
-    <button class="btn btn--right">
+    <button class="btn btn--right" @click="nextTestimonial">
       <PhCaretRight :size="32" weight="light" class="btn-icon"/>
     </button>
   </div>
@@ -22,7 +22,47 @@ import {PhCaretLeft, PhCaretRight} from "@phosphor-icons/vue";
 
 export default {
   name: "Testimonials",
-  components: {PhCaretRight, PhCaretLeft}
+  components: {PhCaretRight, PhCaretLeft},
+  data() {
+    return {
+      currentTestimonial: 0,
+      testimonials: [
+        {
+          img: require("../assets/img/testimonial/testimonial1.png"),
+          alt: "Portrait of client.",
+          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lorem velit, vulputate eu lacus ut, dictum tempor.",
+          name: "Mari Maasikas",
+        },
+        {
+          img: require("../assets/img/testimonial/testimonial2.png"),
+          alt: "Portrait of client.",
+          text: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+          name: "Liina Lill",
+        },
+        {
+          img: require("../assets/img/testimonial/testimonial3.png"),
+          alt: "Portrait of client.",
+          text: "Aliquam tincidunt mauris eu risus. Vivamus egestas tristique nunc.",
+          name: "Mai Kuu",
+        },
+      ],
+    }
+  },
+  methods: {
+    nextTestimonial() {
+      this.currentTestimonialIndex = (this.currentTestimonialIndex + 1) % this.testimonials.length;
+    },
+    previousTestimonial() {
+      this.currentTestimonialIndex =
+          (this.currentTestimonialIndex - 1 + this.testimonials.length) % this.testimonials.length;
+    },
+    startAutoRotate() {
+      setInterval(this.nextTestimonial, 5000);
+    },
+  },
+  mounted() {
+    this.startAutoRotate();
+  },
 }
 </script>
 
@@ -63,7 +103,7 @@ export default {
   outline: none;
 }
 
-.btn:hover{
+.btn:hover {
   color: #9B89A9;
 }
 
