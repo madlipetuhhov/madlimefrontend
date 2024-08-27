e984c351f0dc5d966ee7e9c3f81560f07827b67d
Madli <madli.petuhhov89@gmail.com>
2024-08-27 12:04:21 +0300

    accordion and styles wip

--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -1,19 +1,30 @@
 <template>
   <div class="accordion">
-    <div class="accordion-item" v-for="(item, index) in accordionItems" :key="index">
-      <div class="" @click="openSection(index)">
-        <h3 class="heading-tertiary">{{ item.title }}</h3>
-      </div>
-      <div v-show="activeIndex === index" class="accordion-content">
-        <p>{{ item.content }}</p>
+    <div class="container">
+      <div class="accordion-item" v-for="(item, index) in accordionItems" :key="index">
+        <div class="accordion-header" @click="openSection(index)">
+          <h3 class="heading-tertiary">{{ item.title }}</h3>
+          <button v-if="activeIndex !== index" class="caret caret--down">
+            <PhCaretDown :size="32" weight="light" class="caret-icon"/>
+          </button>
+          <button v-if="activeIndex === index" class="caret caret--up">
+            <PhCaretUp :size="32" weight="light" class="caret-icon"/>
+          </button>
+        </div>
+        <div v-show="activeIndex === index" class="accordion-text">
+          <p>{{ item.content }}</p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import {PhCaretDown, PhCaretUp} from "@phosphor-icons/vue";
+
 export default {
   name: "Accordion",
+  components: {PhCaretUp, PhCaretDown},
   props: {
     accordionItems: {
       type: Array,
@@ -37,14 +48,22 @@ export default {
 .accordion-item {
   margin-bottom: 10px;
 }
+
 .accordion-header {
-  padding: 10px;
-  background-color: #f7f7f7;
-  cursor: pointer;
-  border-bottom: 1px solid #ccc;
+  position: relative;
+  background-color: #F9F8FA;
+  padding: 1.6rem;
 }
-.accordion-content {
-  padding: 10px;
-  background-color: #fff;
+
+.accordion-text {
+  font-size: 1.8rem;
+  line-height: 1.5;
+  padding: 1.6rem 6.4rem 1.6rem 1.6rem;
+}
+
+.caret--down, .caret--up {
+  top: 50%;
+  right: 1.6rem;
+  transform: translateY(-50%);
 }
 </style>
