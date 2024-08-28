be77937214e2b17b0884e4c140110279e82fd781
Madli <madli.petuhhov89@gmail.com>
2024-08-28 18:19:28 +0300

    navbar done

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import PortfolioView from "@/views/PortfolioView.vue";
 import AboutView from "@/views/AboutView.vue";
@@ -7,41 +7,46 @@ import ContactView from "@/views/ContactView.vue";
 import ConditionsView from "@/views/ConditionsView.vue";
 
 const routes = [
-  {
-    path: '/',
-    name: 'homeRoute',
-    component: HomeView
-  },
-  {
-    path: '/portfoolio',
-    name: 'portfolioRoute',
-    component: PortfolioView
-  },
-  {
-    path: '/minust',
-    name: 'aboutRoute',
-    component: AboutView
-  },
-  {
-    path: '/hind',
-    name: 'priceRoute',
-    component: PriceView
-  },
-  {
-    path: '/kontakt',
-    name: 'contactRoute',
-    component: ContactView
-  },
-  {
-    path: '/tingimused',
-    name: 'conditionsRoute',
-    component: ConditionsView
-  },
+    {
+        path: '/',
+        name: 'homeRoute',
+        component: HomeView
+    },
+    {
+        path: '/portfoolio',
+        name: 'portfolioRoute',
+        component: PortfolioView
+    },
+    {
+        path: '/minust',
+        name: 'aboutRoute',
+        component: AboutView
+    },
+    {
+        path: '/hind',
+        name: 'priceRoute',
+        component: PriceView
+    },
+    {
+        path: '/kontakt',
+        name: 'contactRoute',
+        component: ContactView
+    },
+    {
+        path: '/tingimused',
+        name: 'conditionsRoute',
+        component: ConditionsView
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    window.scrollTo(0, 0)
+    next()
 })
 
 export default router
